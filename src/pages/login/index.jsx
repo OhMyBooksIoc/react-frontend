a2379ee695700c57db9b4ebbb4cd7cafc180641a
mcmontseny <mcmontseny.dev@gmail.com>
2022-11-09 21:21:48 +0100

    Fix all styles login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,11 +3,7 @@ import { useForm } from "react-hook-form";
 import "./styles.scss";
 
 function LoginPage() {
-
-  const {
-    register,
-    handleSubmit,
-  } = useForm({
+  const { register, handleSubmit } = useForm({
     mode: "onTouched",
   });
 
@@ -23,83 +19,84 @@ function LoginPage() {
       body: JSON.stringify(body),
     };
 
-    const response = await fetch('http://localhost:8080/user/login', requestOptions);
+    const response = await fetch(
+      "http://localhost:8080/user/login",
+      requestOptions
+    );
     const { status, user } = await response.json();
 
     if (status === "200 OK") {
-      localStorage.setItem('user', JSON.stringify(user));
-      localStorage.setItem('isAuthenticated', true);
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("isAuthenticated", true);
       window.location.href = "/my-account";
     }
   };
 
-
   return (
-    <div className="login">
-      <div className="login__header">
-        <h1 className="login__header__title">Iniciar sesión</h1>
-        <span className="login__header__subtitle">Bienvenido a OhMyBooks!</span>
-      </div>
-      <div className="login__content">
-        <form className="login__content__form" onSubmit={handleSubmit(onSubmit)}>
-          <div className="login__content__form__item">
-            <label
-              className="login__content__form__item__label"
-              htmlFor="email"
-            >
-              Correo electrónico
-            </label>
+    <div className="container">
+      <div className="login">
+        <div className="login__header">
+          <span className="login__header__pretitle">Benvingut de nou!</span>
+          <h1 className="login__header__title">Iniciar Sessió</h1>
+        </div>
+        <div className="login__content">
+          <form
+            className="login__content__form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="login__content__form__item">
+              <label
+                className="login__content__form__item__label"
+                htmlFor="email"
+              >
+                Correu electrònic
+              </label>
 
-            <input
-              className="login__content__form__item__input"
-              id="email"
-              type="email"
-              placeholder="Correo electrónico"
-              required
-              {...register("email", { required: true })}
-            />
-          </div>
-          <div className="login__content__form__item">
-            <label
-              className="login__content__form__item__label"
-              htmlFor="password"
-            >
-              Contraseña
-            </label>
+              <input
+                className="login__content__form__item__input"
+                id="email"
+                type="email"
+                required
+                {...register("email", { required: true })}
+              />
+            </div>
+            <div className="login__content__form__item">
+              <label
+                className="login__content__form__item__label"
+                htmlFor="password"
+              >
+                Contrasenya
+              </label>
 
-            <input
-              className="login__content__form__item__input"
-              placeholder="Contraseña"
-              id="password"
-              type="password"
-              required
-              {...register("password", { required: true })}
-            />
+              <input
+                className="login__content__form__item__input"
+                id="password"
+                type="password"
+                required
+                {...register("password", { required: true })}
+              />
+            </div>
+            <button className="login__content__form__button">
+              Iniciar Sessió
+            </button>
+            <a
+              className="login__content__form__recovery-pwd"
+              href="/es/es/password-reset/"
+              tabindex="3"
+            >
+              Has oblidat la teva contrasenya?
+            </a>
+          </form>
+          <div className="login__content__register">
+            <span>Encara no tens compte?</span>
+            <a
+              className="login__content__register__link"
+              href="/register"
+              tabindex="6"
+            >
+              Registra't
+            </a>
           </div>
-          <a
-            className="login__content__form__recovery-pwd"
-            href="/es/es/password-reset/"
-            tabindex="3"
-          >
-            ¿Has olvidado tu contraseña?
-          </a>
-          <button
-            className="login__content__form__button"
-            type="submit"
-            tabindex="4"
-          >
-            <span>Iniciar sesión</span>
-          </button>
-        </form>
-        <div className="login__content__register">
-          <span>¿Aún no tienes cuenta?</span>
-          <a
-            className="login__content__register__link"
-            href="/register"
-            tabindex="6"
-          >
-            Regístrate
-          </a>
         </div>
       </div>
     </div>
