f862f0e19e1e0ce8073bbc758bed3cb6b796a5b9
mcmontseny <mcmontseny.dev@gmail.com>
2022-11-12 15:30:24 +0100

    Fix text pages

--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -1,12 +1,14 @@
+import "./styles.scss";
+
 function AboutUsPage() {
     return (
-      <div className="text-container">
-        <h1 className="text-container__header">Qui som?</h1>
-        <h2 className="text-container__title">La lectura ens mou</h2>
-        <p className="text-container__text">OhMyBooks neix amb el propòsit de permetre una organització adequada de la nostra col·lecció de llibres. <br />
+      <div className="about-us">
+        <h1 className="about-us__header">Qui som?</h1>
+        <h2 className="about-us__title">La lectura ens mou</h2>
+        <p className="about-us__text">OhMyBooks neix amb el propòsit de permetre una organització adequada de la nostra col·lecció de llibres. <br />
           Volem ajudar-te a portar sempre amb tu la teva preuada col·lecció i portar el compte de les teves lectures pendents. Volem agermanar aficionats a la lectura oferint la possibilitat de retroalimentar i inspirar els uns als altres a obrir-se a noves lectures.</p>
-        <h2 className="text-container__title">Tot comença per alguna cosa</h2>
-        <p className="text-container__text">Com tot bon llibre com cal, OhMyBooks té el seu plantejament, nus i desenllaç.<br />
+        <h2 className="about-us__title">Tot comença per alguna cosa</h2>
+        <p className="about-us__text">Com tot bon llibre com cal, OhMyBooks té el seu plantejament, nus i desenllaç.<br />
           Aquesta història comença amb quatre persones que han de fer un projecte de fi de grau de Desenvolupament d'Aplicacions Web, però això no és l'única cosa que tenien en comú, ja que també els unia la passió per la lectura. Així que vam posar fil a l'agulla. Això ens porta a que l'únic desenllaç possible és que tinguis la millor experiència gestionant la teva col·lecció de llibres.</p>
       </div>
     );
