cb09b5dabb50f592b0dd944957a755a847efeacf
mcmontseny <mcmontseny.dev@gmail.com>
2022-11-13 21:15:35 +0100

    Add profile pic header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,7 +8,7 @@ import "./styles.scss";
 
 const isAuthenticated = !!(localStorage.getItem("isAuthenticated") || false);
 
-function MenuItems() {
+function ProfilePic({ isDesktop }) {
   const [userIsHover, setUserIsHover] = useState(false);
   const [userMenuIsHover, setUserMenuIsHover] = useState(false);
 
@@ -27,6 +27,52 @@ function MenuItems() {
   };
 
   return (
+    <>
+      <a
+        href={isDesktop ? "/my-account" : null}
+        onMouseEnter={() => setUserIsHover(true)}
+        onMouseLeave={() => setTimeout(() => setUserIsHover(false), 200)}
+      >
+        <div className="header__container__items__profile-pic" />
+      </a>
+      {userIsHover ? (
+        <div
+          id="prova"
+          className="header__container__items__profile-options"
+          onMouseEnter={() => {
+            setUserIsHover(true);
+            setUserMenuIsHover(true);
+          }}
+          onMouseLeave={() => {
+            setUserMenuIsHover(false);
+            setUserIsHover(false);
+          }}
+        >
+          <div className="header__container__items__profile-options__title">
+            Hola Lore
+          </div>
+          <div className="header__container__items__profile-options__links">
+            <a
+              href="/my-account"
+              className="header__container__items__profile-options__links__link"
+            >
+              Compte
+            </a>
+            <button
+              className="header__container__items__profile-options__links__link__logout"
+              onClick={() => logout()}
+            >
+              Tancar Sessió
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+function MenuItems({ isDesktop }) {
+  return (
     <div className="header__container__items">
       <a href="/" className="header__container__items__item">
         Inici
@@ -39,53 +85,12 @@ function MenuItems() {
           <a href="/register" className="header__container__items__item">
             Registre
           </a>
-          <a
-            href="/login"
-            className="header__container__items__button"
-            onClick={logout}
-          >
+          <a href="/login" className="header__container__items__button">
             Inicia sessió
           </a>
         </>
       ) : (
-        <>
-          <a
-            href="/my-account"
-            onMouseEnter={() => setUserIsHover(true)}
-            onMouseLeave={() => setTimeout(() => setUserIsHover(false), 200)}
-          >
-            <div className="header__container__items__profile-pic" />
-          </a>
-          {userIsHover ? (
-            <div
-              id="prova"
-              className="header__container__items__profile-options"
-              onMouseEnter={() => {
-                setUserIsHover(true);
-                setUserMenuIsHover(true);
-              }}
-              onMouseLeave={() => {
-                setUserMenuIsHover(false);
-                setUserIsHover(false);
-              }}
-            >
-              <div className="header__container__items__profile-options__title">
-                Hola Lore
-              </div>
-              <div className="header__container__items__profile-options__links">
-                <a
-                  href="/my-account"
-                  className="header__container__items__profile-options__links__link"
-                >
-                  Compte
-                </a>
-                <button className="header__container__items__profile-options__links__link__logout" onClick={() => logout()}>
-                  Tancar Sessió
-                </button>
-              </div>
-            </div>
-          ) : null}
-        </>
+        <>{isDesktop ? <ProfilePic {...{ isDesktop }} /> : null}</>
       )}
     </div>
   );
@@ -108,17 +113,23 @@ function Header() {
   return (
     <header className="header">
       <div className="header__container">
-        {!isDesktop ? (
-          <FontAwesomeIcon
-            icon={!menuIsOpen ? faBars : faTimes}
-            onClick={() => setMenuIsOpen(!menuIsOpen)}
-          />
-        ) : null}
+        <div className="header-left">
+          {!isDesktop ? (
+            <FontAwesomeIcon
+              icon={!menuIsOpen ? faBars : faTimes}
+              onClick={() => setMenuIsOpen(!menuIsOpen)}
+            />
+          ) : null}
+          <a className="header__container__logo" href="/">
+            <img src="./images/logo.png" />
+          </a>
+        </div>
 
-        <a className="header__container__logo" href="/">
-          <img src="./images/logo.png" />
-        </a>
-        {isDesktop ? <MenuItems /> : null}
+        {isDesktop ? (
+          <MenuItems {...{ isDesktop }} />
+        ) : (
+          <> {isAuthenticated ? <ProfilePic {...{ isDesktop }} /> : null} </>
+        )}
       </div>
       {menuIsOpen && !isDesktop ? (
         <div
@@ -126,7 +137,7 @@ function Header() {
             menuIsOpen ? "header__mobile--open" : ""
           }`}
         >
-          <MenuItems />
+          <MenuItems {...{ isDesktop }} />
         </div>
       ) : null}
     </header>
